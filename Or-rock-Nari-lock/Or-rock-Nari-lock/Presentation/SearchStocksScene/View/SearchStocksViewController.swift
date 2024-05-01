3dd27d64f9efc65d10aa8a8e949b795a5a77ea5d
chopmozzi <chopmojji@gmail.com>
2024-05-01 12:18:47 +0900

    :sparkles: 주식 결과 검색 ViewController 추가

---
+++ Or-rock-Nari-lock/Or-rock-Nari-lock/Presentation/SearchStocksScene/View/SearchStocksViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchStocksViewController.swift
+//  Or-rock-Nari-lock
+//
+//  Created by 황지웅 on 5/1/24.
+//
+
+import UIKit
+
+final class SearchStocksViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
